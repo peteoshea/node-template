29320aff5095008d6d4904ad9d02cdfae5cdc97b
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-02 16:53:07 +0100

    npm doesn't always seem to work first time from a powershell script
    The call to where seems to help it work but just to double check get it to return the version number as well.

--- powershell/bin/bootstrap.ps1
+++ powershell/bin/bootstrap.ps1
@@ -71,5 +71,10 @@ if (Test-Path -Path "$packageJson" -PathType Leaf) {
     }
 
     Write-Host "`n==> Installing npm dependencies..."
+    Write-Host "Where is npm running from:"
+    where.exe npm
+    Write-Host "What version of npm:"
+    npm -v
+    Write-Host "Install npm packages..."
     npm install
 }
